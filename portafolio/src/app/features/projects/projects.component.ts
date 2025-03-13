579620c6d3fbab3cce2afc24acd9acb6d17509f8
YoseC <yo.cifuentes@duocuc.cl>
2025-03-13 13:30:42 -0300

    🎨 Rediseñar la sección de inicio y proyectos con nuevos estilos y contenido visual

--- portafolio/src/app/features/projects/projects.component.ts
+++ portafolio/src/app/features/projects/projects.component.ts
@@ -12,9 +12,8 @@ import { CommonModule } from '@angular/common';
 })
 export class ProjectsComponent {
   projects = [
-    { name: 'Proyecto 1', description: 'Descripción del proyecto 1', link: '#' },
-    { name: 'Proyecto 2', description: 'Descripción del proyecto 2', link: '#' },
-    { name: 'Proyecto 3', description: 'Descripción del proyecto 3', link: '#' }
+    { name: 'Proyecto 1', description: 'Descripción del proyecto 1', image: 'https://source.unsplash.com/300x200/?code', link: '#' },
+    { name: 'Proyecto 2', description: 'Descripción del proyecto 2', image: 'https://source.unsplash.com/300x200/?app', link: '#' },
+    { name: 'Proyecto 3', description: 'Descripción del proyecto 3', image: 'https://source.unsplash.com/300x200/?website', link: '#' }
   ];
 }
-
